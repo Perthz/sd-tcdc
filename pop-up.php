d9131c40fded9bfbeca43c8fa5715fe18d383f0d
narinnop <nnarin.nop@gmail.com>
2016-12-23 00:36:33 +0700

    preview

--- pop-up.php
+++ pop-up.php
@@ -9,14 +9,10 @@
 			<?php include 'service-design-process.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
 		</div>
 	</div>
-	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="brief" role="dialog">
-		<?php include 'brief.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
-	</div>
+
+
 	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="initiate" role="dialog">
 		<?php include 'initiate.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
-	</div>
-	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="discover" role="dialog">
-		<?php include 'discover.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
 	</div>
 	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="trendmatrix" role="dialog">
 		<?php include 'trend-matrix.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
@@ -27,20 +23,102 @@
 	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="intent-statement" role="dialog">
 		<?php include 'intent-statement.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
 	</div>
+	
+	
 	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="discover" role="dialog">
 		<?php include 'discover.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
 	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="contextual-interview" role="dialog">
+		<?php include 'contextual-interview.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="poems-framework" role="dialog">
+		<?php include 'poems-framework.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="image-sorting" role="dialog">
+		<?php include 'image-sorting.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="mobile-ethnography" role="dialog">
+		<?php include 'mobile-ethnography.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="remote-research" role="dialog">
+		<?php include 'remote-research.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	
+
 	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="define" role="dialog">
 		<?php include 'define.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
 	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="observation-to-insights" role="dialog">
+		<?php include 'observation-to-insights.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="stakeholder-map" role="dialog">
+		<?php include 'stakeholder-map.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="persona" role="dialog">
+		<?php include 'persona.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="user-journey-map" role="dialog">
+		<?php include 'user-journey-map.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="design-principles-generation" role="dialog">
+		<?php include 'design-principles-generation.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="brief" role="dialog">
+		<?php include 'brief.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="design-brief" role="dialog">
+		<?php include 'design-brief.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+
+
 	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="develop" role="dialog">
 		<?php include 'develop.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
 	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="co-creation" role="dialog">
+		<?php include 'co-creation.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="morphological-chart" role="dialog">
+		<?php include 'morphological-chart.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="concept-generation-matrix" role="dialog">
+		<?php include 'concept-generation-matrix.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="solution-diagramming" role="dialog">
+		<?php include 'solution-diagramming.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="scenario-and-story-telling" role="dialog">
+		<?php include 'scenario-and-story-telling.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	
 	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="deliver" role="dialog">
 		<?php include 'deliver.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
 	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="pugh-matrix" role="dialog">
+		<?php include 'pugh-matrix.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="behavioral-prototyping" role="dialog">
+		<?php include 'behavioral-prototyping.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="semantic-differential-scale" role="dialog">
+		<?php include 'semantic-differential-scale.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="service-blueprint" role="dialog">
+		<?php include 'service-blueprint.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="business-model-canvas" role="dialog">
+		<?php include 'business-model-canvas.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+
+
 	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="launch" role="dialog">
 		<?php include 'launch.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
 	</div>
+	<div aria-hidden="true" aria-labelledby="videoModalTitle" class="reveal large" data-reveal="" id="consumer-response-model" role="dialog">
+		<?php include 'consumer-response-model.html';?><a class="close-button" data-close=""><img src="assets/close-icon.svg"></a>
+	</div>
+
+
 </body>
 </html>
