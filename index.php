bb3966ed7ac7fde7f10b248695747abf1804ee70
weerawat weera <i@phz.me>
2016-12-23 00:12:39 +0700

    Cur Orbit Container

--- index.php
+++ index.php
@@ -82,16 +82,12 @@
       <div class="large-7">
           
             <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
-              <ul class="orbit-container">
 <!--
                 <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
                 <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
 -->
-                <li class="is-active orbit-slide">
                   <a href="#" data-open="heroModal"><img class="" src="assets/double-diamond.svg" alt="Diagram"></a>
-                </li>
                
-              </ul>
                <!--<nav class="orbit-bullets">
                 <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
                 <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
